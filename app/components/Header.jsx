9a6d26838a2056643f0f41a2019cd826f5802b48
yeasinChowdhury <depressedman996@gmail.com>
2025-08-15 20:55:18 +0600

    first commit

---
+++ app/components/Header.jsx
@@ -0,0 +1,46 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className=" text-black w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
+      <div>
+        <Image
+          src={assets.myprofile_img}
+          alt="myprofile_img"
+          className="rounded-full h-32 w-32"
+        />
+      </div>
+      <h3 className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo">
+        Hi! I'm Yeasin Chowdhuriy
+        <Image src={assets.hand_icon} alt="" className="w-6" />
+      </h3>
+      <h1 className="text-3xl sm:text-6xl lg:text-[-66px] font-Ovo">
+        frontend web developer based in Bangaldesh
+      </h1>
+      <p className="max-w-2xl mx-auto font-Ovo">
+        I am a Frontedn developer from Bangaldesh ,Dhaka with 1 year of
+        experience in multiple caomanies like brain starion{" "}
+      </p>
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
+        <a
+          href="#contact"
+          className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent"
+        >
+          contact me
+          <Image src={assets.right_arrow_white} alt="" className="w-4" />
+        </a>
+        <a
+          href="/sample-resume.pdf"
+          download
+          className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
+        >
+          my resume <Image src={assets.download_icon} alt="" className="w-4" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,177 @@
+"use client";
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
+
+const Navbar = ({ isDarkMode, setIsDarkMode }) => {
+  const [isScroll, setScrool] = useState(false);
+  const sideMenuRef = useRef();
+
+  const openMenu = () => {
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(-16rem)";
+    }
+  };
+
+  const closeMenu = () => {
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(16rem)";
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (scrollY > 50) {
+        setScrool(true);
+      } else {
+        setScrool(false);
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      {/* Background Image */}
+      <div className=" text-black fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden">
+
+        <Image
+          src={assets.header_bg_color}
+          alt="Header Background"
+          className="w-full"/>
+      </div>
+
+      {/* Navbar */}
+      <nav
+        className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${
+          isScroll
+            ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20"
+            : ""
+        }`}
+      >
+        {/* Logo */}
+        <a href="#top">
+          <div className="text-bold text-3xl">
+            
+            Yeasin CHowdhuriy
+          </div>
+          {/* <Image
+            src={isDarkMode ? assets.logo_dark : assets.logo}
+            alt="Logo"
+            className="w-8 cursor-pointer mr-14"
+          /> */}
+        </a>
+
+        {/* Desktop Menu */}
+        <ul
+          className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${
+            isScroll
+              ? ""
+              : "bg-white shadow-sm bg-opacity-50  dark:border dark:border-white/50 dark:bg-transparent"
+          }  `}
+        >
+          <li>
+            <a className="font-Ovo" href="#top">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#about">
+              About me
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#services">
+              Services
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#work">
+              My Work
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#contact">
+              Contact me
+            </a>
+          </li>
+        </ul>
+
+        {/* Right Buttons */}
+        <div className="flex gap-4 items-center">
+          {/* Dark Mode Button */}
+          <button onClick={() => setIsDarkMode((prev) => !prev)}>
+            <Image
+              src={isDarkMode ? assets.sun_icon : assets.moon_icon}
+              alt="Moon Icon"
+              className="w-6"
+            />
+          </button>
+
+          {/* Contact Button */}
+          <a
+            href="#contact"
+            className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-teal-500 rounded-full ml-4 font-Ovo dark:border-white/50"
+          >
+            Contact
+            <Image
+              src={isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon}
+              alt=""
+              className="w-3"
+            />
+          </a>
+
+          {/* Mobile Menu Open Button */}
+          <button className="block md:hidden ml-3" onClick={openMenu}>
+            <Image
+              src={isDarkMode ? assets.menu_white : assets.menu_black}
+              alt=""
+              className="w-6"
+            />
+          </button>
+        </div>
+
+        {/* Mobile Side Menu */}
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-darkHover dark:text-white"
+        >
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={isDarkMode ? assets.close_white : assets.close_black}
+              alt="Close Icon"
+              className="w-5 cursor-pointer"
+            />
+          </div>
+
+          <li>
+            <a className="font-Ovo" href="#top">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#about">
+              About me
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#services">
+              Services
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#work">
+              My Work
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#contact">
+              Contact me
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
